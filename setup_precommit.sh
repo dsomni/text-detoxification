7c28085c738b786ed1e0abecdcc8f4ee862bba79
Dmitry <pro100pro10010@gmail.com>
2023-10-30 20:52:04 +0300

    Update [pre-commit hook] setup_precommit.sh

--- setup_precommit.sh
+++ setup_precommit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "#!/bin/sh
 pipenv run pip freeze > requirements.txt
-pipenv run black .
+pipenv run ruff format .
 pipenv run ruff check . --fix --exit-non-zero-on-fix
-git add -A" > .git/hooks/pre-commit
+FILES=\`git diff --name-only --cached\`
+git add \$FILES requirements.txt" > .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
